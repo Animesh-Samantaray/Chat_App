bf1f5e242f49e5de08ee405259f09a954b89cb00
Animesh Samantaray <animeshsamantaray2@gmail.com>
2025-10-20 11:57:20 +0530

    updated

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -33,8 +33,8 @@ const ChatContainer = () => {
     <div className="flex flex-col h-full bg-[#0f0f0f] text-white">
       <ChatHeader />
 
-      <div>
-        
+      <div className="flex-1 overflow-y-auto p-4 ">
+
       </div>
 
       {/* Message Input */}
