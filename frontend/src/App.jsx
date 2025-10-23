caaa8e04c8bd564bcea1801157bfa4686bd0fed8
Animesh Samantaray <animeshsamantaray2@gmail.com>
2025-10-23 11:11:34 +0530

    bug removed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useState,useEffect } from 'react';
 import Navbar from './components/Navbar.jsx'
 import HomePage from './pages/HomePage.jsx';
 import {Routes,Route} from 'react-router-dom';
-import SignUpPage from './pages/signUpPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
 import SettingsPage from './pages/SettingsPage.jsx';
